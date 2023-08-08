ee49317179eb86c6cab3c23c20cacc7615760426
Anmol <as0796348@gmail.com>
2023-08-08 23:21:26 +0530

    Routing added

--- src/components/header.js
+++ src/components/header.js
@@ -1,41 +1,19 @@
 import { Link } from "react-router-dom";
 import "../components/header.css";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import About from "../components/pages/About";
-// import Blogs from "../components/pages/Blogs"
-// import Contact from "../components/pages/Contact"
-// import Features from "../components/pages/Features"
-// import Pricing from "../components/pages/Pricing"
-// import Trainers from "../components/pages/Trainers"
 
 const Header = () => {
     return (
         <div className="nav-container">
             <nav className="navbars">
                 <ul>
-                    <li>
-                        <Link to="/About">About Us</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/Blogs">Blogs</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/Features">Features</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/Pricing">Pricing</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/Trainers">Trainers</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/Contact">Contact</Link>
-                    </li>
+                    <Link to="/Home">Home</Link>
+                    <Link to="/About">About Us</Link>
+                    <Link to="/Blogs">Blogs</Link>
+                    <Link to="/Features">Features</Link>
+                    <Link to="/Pricing">Pricing</Link>
+                    <Link to="/Trainers">Trainers</Link>
+                    <Link to="/Contact">Contact</Link>
                 </ul>
             </nav>
         </div>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,10 @@
+import React from 'react';
+
 const About = () => {
     return (
-        <h1>About Page</h1>
+        <>
+            <h1>About Page</h1>
+        </>
     )
 }
 export default About;
