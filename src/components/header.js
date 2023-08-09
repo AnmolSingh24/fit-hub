dd68fe5a306902e30c45725855ffdd883ed70510
Anmol <as0796348@gmail.com>
2023-08-09 14:38:09 +0530

    Chnages updated

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 import "../components/header.css";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Header = () => {
     return (
         <div className="nav-container">
+            <header className='header'>
+                <a href='#' className='logo'><span>FIT</span>HUB</a>
+            </header>
             <nav className="navbars">
                 <ul>
-                    <Link to="/Home">Home</Link>
-                    <Link to="/About">About Us</Link>
-                    <Link to="/Blogs">Blogs</Link>
-                    <Link to="/Features">Features</Link>
-                    <Link to="/Pricing">Pricing</Link>
-                    <Link to="/Trainers">Trainers</Link>
-                    <Link to="/Contact">Contact</Link>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About Us</Link>
+                    <Link to="/blogs">Blogs</Link>
+                    <Link to="/features">Features</Link>
+                    <Link to="/pricing">Pricing</Link>
+                    <Link to="/trainers">Trainers</Link>
+                    <Link to="/contact">Contact</Link>
                 </ul>
             </nav>
         </div>
