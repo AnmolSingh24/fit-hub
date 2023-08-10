3cba234b7a7550b08ee600bb02770c3a1e9f2f62
Anmol <as0796348@gmail.com>
2023-08-10 13:01:29 +0530

    About us completed

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <>
-            <h1>About Page</h1>
-        </>
+        <div className='about-container'>
+            <div className='about-content'>
+                <img src='https://res.cloudinary.com/djdblxcxt/image/upload/v1691650697/about-img_rkwpqt.jpg' />
+                <div className='info'>
+                    <h2>About Us</h2>
+                    <h1>Everyday is a chance to become better</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. <br /> Curabitur id tellus ac libero lacinia vestibulum.
+                        Fusce quis justo vitae<br /> magna laoreet tristique. Aenean auctor, risus non fringilla placerat,<br /> turpis velit tincidunt odio.
+                    </p>
+                </div>
+            </div>
+
+            <div className='info-container'>
+                <div className='info-items'>
+                    <div className='info-name'>
+                        <b>Body and Mind</b>
+                        <p>Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Nulla facilisi.</p>
+                    </div>
+
+                    <div className='info-name'>
+                        <b>Healthy Life</b>
+                        <p>Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Nulla facilisi.</p>
+                    </div>
+                </div>
+
+                <div className='info-items'>
+                    <div className='info-name'>
+                        <b>Strategies</b>
+                        <p>Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Nulla facilisi.</p>
+                    </div>
+
+                    <div className='info-name'>
+                        <b>Workout</b>
+                        <p>Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Nulla facilisi.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default About;
