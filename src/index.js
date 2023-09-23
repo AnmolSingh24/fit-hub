c60a237d32e759dcff9535b4bb2e32a57b98bc0b
Anmol <as0796348@gmail.com>
2023-09-23 21:08:06 +0530

    comments removed

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById('root')
-// );
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
